3dccf3e27a480d2e2b865954366700bde780129c
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-06 19:16:54 -0400

    wrapper added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/header"
 import Footer from "./components/Footer/footer"
+import Wrapper from "./components/Wrapper/wrapper"
 
 
 function App() {
   return (
     <>
       <Header />
+      <Wrapper>
+      </Wrapper>
       <Footer />
     </>
   );
