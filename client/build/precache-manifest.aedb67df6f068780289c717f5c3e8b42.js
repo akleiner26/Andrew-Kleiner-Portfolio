2680df7ea9cd07a4397da75eb384398276f034e5
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-10 20:38:31 -0400

    build folder added

---
+++ client/build/precache-manifest.aedb67df6f068780289c717f5c3e8b42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4e00652f35eb3c244a1bf5ccb3299b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fe8eed862f474a5c4b0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2b8a9e0041e3bf849254",
+    "url": "/static/css/main.a297dff3.chunk.css"
+  },
+  {
+    "revision": "9fe8eed862f474a5c4b0",
+    "url": "/static/js/2.358f5ed2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.358f5ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8a9e0041e3bf849254",
+    "url": "/static/js/main.b1139fd6.chunk.js"
+  },
+  {
+    "revision": "a4711610b4b306460af8",
+    "url": "/static/js/runtime-main.0c75be4d.js"
+  }
+]);
