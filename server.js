06ff6a8cb1dc013f4ed59bc586cae1c5fb10230a
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-10 21:35:40 -0400

    path added to server

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ require("dotenv").config()
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.get("*", function (req,res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
